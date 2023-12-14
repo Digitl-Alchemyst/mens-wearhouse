b17b1033f598bf7d4487677eacc4137550820c89
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2023-12-13 23:39:53 -0600

    Data

--- constants.js
+++ constants.js
@@ -55,3 +55,267 @@ export const categorySlider = [
         url: '/tie',
     },
 ]
+
+export const brandsWeLove = [
+  {
+    id: 1,
+    title: 'Paisley & Gray',
+    img: 'paisley_gray.svg',
+    url: 'paisley-gray',
+  },
+  {
+    id: 2,
+    title: 'Joseph Abboud',
+    img: 'joseph_abboud.svg',
+    url: 'abboud',
+  },
+  {
+    id: 3,
+    title: 'Kenneth Sole',
+    img: 'kenneth_cole.svg',
+    url: 'sole',
+  },
+  {
+    id: 4,
+    title: 'Calvin Klein',
+    img: 'calvin_klein.svg',
+    url: 'klein',
+  },
+  {
+    id: 5,
+    title: 'Michael Strahn',
+    img: 'michael_strahan.svg',
+    url: 'strahn',
+  },
+];
+
+export const bestSellingSuits = [
+  {
+    id: 1,
+    title: 'Paisley & Gray',
+    img: 'paisley_gray.svg',
+    url: 'paisley-gray',
+  },
+  {
+    id: 2,
+    title: 'Joseph Abboud',
+    img: 'joseph_abboud.svg',
+    url: 'abboud',
+  },
+  {
+    id: 3,
+    title: 'Kenneth Sole',
+    img: 'kenneth_cole.svg',
+    url: 'sole',
+  },
+  {
+    id: 4,
+    title: 'Calvin Klein',
+    img: 'calvin_klein.svg',
+    url: 'klein',
+  },
+  {
+    id: 5,
+    title: 'Michael Strahn',
+    img: 'michael_strahan.svg',
+    url: 'strahn',
+  },
+];
+
+export const bestSellingShirts = [
+  {
+    id: 1,
+    title: 'Paisley & Gray',
+    img: 'paisley_gray.svg',
+    url: 'paisley-gray',
+  },
+  {
+    id: 2,
+    title: 'Joseph Abboud',
+    img: 'joseph_abboud.svg',
+    url: 'abboud',
+  },
+  {
+    id: 3,
+    title: 'Kenneth Sole',
+    img: 'kenneth_cole.svg',
+    url: 'sole',
+  },
+  {
+    id: 4,
+    title: 'Calvin Klein',
+    img: 'calvin_klein.svg',
+    url: 'klein',
+  },
+  {
+    id: 5,
+    title: 'Michael Strahn',
+    img: 'michael_strahan.svg',
+    url: 'strahn',
+  },
+];
+
+export const bestSellingPants = [
+  {
+    id: 1,
+    title: 'Paisley & Gray',
+    img: 'paisley_gray.svg',
+    url: 'paisley-gray',
+  },
+  {
+    id: 2,
+    title: 'Joseph Abboud',
+    img: 'joseph_abboud.svg',
+    url: 'abboud',
+  },
+  {
+    id: 3,
+    title: 'Kenneth Sole',
+    img: 'kenneth_cole.svg',
+    url: 'sole',
+  },
+  {
+    id: 4,
+    title: 'Calvin Klein',
+    img: 'calvin_klein.svg',
+    url: 'klein',
+  },
+  {
+    id: 5,
+    title: 'Michael Strahn',
+    img: 'michael_strahan.svg',
+    url: 'strahn',
+  },
+];
+
+export const bestSellingBlazers = [
+  {
+    id: 1,
+    title: 'Paisley & Gray',
+    img: 'paisley_gray.svg',
+    url: 'paisley-gray',
+  },
+  {
+    id: 2,
+    title: 'Joseph Abboud',
+    img: 'joseph_abboud.svg',
+    url: 'abboud',
+  },
+  {
+    id: 3,
+    title: 'Kenneth Sole',
+    img: 'kenneth_cole.svg',
+    url: 'sole',
+  },
+  {
+    id: 4,
+    title: 'Calvin Klein',
+    img: 'calvin_klein.svg',
+    url: 'klein',
+  },
+  {
+    id: 5,
+    title: 'Michael Strahn',
+    img: 'michael_strahan.svg',
+    url: 'strahn',
+  },
+];
+
+export const bestSellingSweaters = [
+  {
+    id: 1,
+    title: 'Paisley & Gray',
+    img: 'paisley_gray.svg',
+    url: 'paisley-gray',
+  },
+  {
+    id: 2,
+    title: 'Joseph Abboud',
+    img: 'joseph_abboud.svg',
+    url: 'abboud',
+  },
+  {
+    id: 3,
+    title: 'Kenneth Sole',
+    img: 'kenneth_cole.svg',
+    url: 'sole',
+  },
+  {
+    id: 4,
+    title: 'Calvin Klein',
+    img: 'calvin_klein.svg',
+    url: 'klein',
+  },
+  {
+    id: 5,
+    title: 'Michael Strahn',
+    img: 'michael_strahan.svg',
+    url: 'strahn',
+  },
+];
+
+export const bestSellingOuterwear = [
+  {
+    id: 1,
+    title: 'Paisley & Gray',
+    img: 'paisley_gray.svg',
+    url: 'paisley-gray',
+  },
+  {
+    id: 2,
+    title: 'Joseph Abboud',
+    img: 'joseph_abboud.svg',
+    url: 'abboud',
+  },
+  {
+    id: 3,
+    title: 'Kenneth Sole',
+    img: 'kenneth_cole.svg',
+    url: 'sole',
+  },
+  {
+    id: 4,
+    title: 'Calvin Klein',
+    img: 'calvin_klein.svg',
+    url: 'klein',
+  },
+  {
+    id: 5,
+    title: 'Michael Strahn',
+    img: 'michael_strahan.svg',
+    url: 'strahn',
+  },
+];
+
+export const bestSellingBoys = [
+  {
+    id: 1,
+    title: 'Paisley & Gray',
+    img: 'paisley_gray.svg',
+    url: 'paisley-gray',
+  },
+  {
+    id: 2,
+    title: 'Joseph Abboud',
+    img: 'joseph_abboud.svg',
+    url: 'abboud',
+  },
+  {
+    id: 3,
+    title: 'Kenneth Sole',
+    img: 'kenneth_cole.svg',
+    url: 'sole',
+  },
+  {
+    id: 4,
+    title: 'Calvin Klein',
+    img: 'calvin_klein.svg',
+    url: 'klein',
+  },
+  {
+    id: 5,
+    title: 'Michael Strahn',
+    img: 'michael_strahan.svg',
+    url: 'strahn',
+  },
+];
